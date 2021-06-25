31ed990c697a65b6ee14d7dc62c2cf4a97810140
Husow070 <71512853+Husow070@users.noreply.github.com>
2021-06-25 11:30:46 +0200

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,30 @@
 const statusS = document.querySelector('.status');
 const reset = document.querySelector ('.reset');
-const cellBOX = document.querySelectorAll('.box')
+const cellBOX = document.querySelectorAll('.box');
+let boardArray = ['', '', '', '', '', '', '', '', ''];
+let player1 = "X";
+let player2 = "O";
+let currentplayer = player1;
+let winner = document.querySelector("#win");
+let winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]];
 
 
-//game variables
-let GameIsCurrent = true;
-let xTurned = true;
+winCombos.forEach((combo) =>{
+    if (boardArray[combo[0]]
+    && boardArray[combo[0]] === boardArray[combo[1]]
+    && boardArray[combo[0]] === boardArray[combo[2]]) {
+    winner = 'current';
+    }
+    });
+
 
 //event handlers
 const handleReset = (e) => {
@@ -14,10 +33,13 @@ const handleReset = (e) => {
 
 const handleBoxClick = (e) => {
     console.log(e);
+    e.target.textContent = "X"
 };
-if (xTurned) {
-    console.log(e.target);
-}
+
+const switchTurn = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
 
 //event listeners
 
@@ -25,4 +47,5 @@ reset.addEventListener("click", handleReset);
 
 for (let i = 0; i < cellBOX.length; i++) {
     cellBOX[i].addEventListener("click", handleBoxClick);
-  }
+}
+
