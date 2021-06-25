b3ef057f40f12baf7b0aef038c7c68af62d067cf
Husow070 <71512853+Husow070@users.noreply.github.com>
2021-06-25 12:41:16 +0200

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,29 +1,20 @@
 const statusS = document.querySelector('.status');
-const reset = document.querySelector ('.reset');
+const reset = document.querySelector('.reset');
 const cellBOX = document.querySelectorAll('.box');
-let boardArray = ['', '', '', '', '', '', '', '', ''];
 let player1 = "X";
 let player2 = "O";
+let playturn = 1;
 let currentplayer = player1;
 let winner = document.querySelector("#win");
 let winCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]];
-
-
-winCombos.forEach((combo) =>{
-    if (boardArray[combo[0]]
-    && boardArray[combo[0]] === boardArray[combo[1]]
-    && boardArray[combo[0]] === boardArray[combo[2]]) {
-    winner = 'current';
-    }
-    });
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]];
 
 
 //event handlers
@@ -33,13 +24,31 @@ const handleReset = (e) => {
 
 const handleBoxClick = (e) => {
     console.log(e);
-    e.target.textContent = "X"
+    if (playturn % 2 == 0) {
+        e.target.textContent = player1;
+    }
+    else if (playturn % 2 == 1) {
+        e.target.textContent = player2;
+    }
+    playturn++;
+    checkWinner();
 };
+
 
 const switchTurn = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
-  };
+};
 
+const checkWinner = () => {
+    winCombos.forEach((combo) => {
+        if (cellBOX[combo[0]].textContent
+            && cellBOX[combo[0]] === cellBOX[combo[1]].textContent
+            && cellBOX[combo[0]] === cellBOX[combo[2]].textContent) {
+            winner = 'cellBOX';
+            console.log("hi")
+        }
+    });
+}
 
 //event listeners
 
